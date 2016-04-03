64620fb0f14a06fd9ac85ad7d567d37bb5cf6516
chandana <chandanashree7@gmail.com>
2016-04-03 00:04:46 -0700

    added division function

--- testProgram.js
+++ testProgram.js
@@ -4,12 +4,18 @@ function sum(num1,num2)
 	console.log("Sum :"+result);
 	return result;
 }
+function division(num1,num2)
+{
+	var result = num1/num2;
+	console.log("Division:"+result);
+	return result;
+}
 function subtract(num1, num2){
 	var result = num2 - num1;
 	console.log("Difference:"+result);
 	return result;
 }
-function multiply(num1,num2)
+function multiplication(num1,num2,num3)
 {
 	var result = num1*num2;
 	console.log("Multiplication:"+result);
