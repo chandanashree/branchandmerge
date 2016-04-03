c211c2b743f7d00aedb180397ca753d5b405424c
chandana <chandanashree7@gmail.com>
2016-04-03 00:08:34 -0700

    modified function parameter

--- testProgram.js
+++ testProgram.js
@@ -4,14 +4,14 @@ function sum(num1,num2)
 	console.log("Sum :"+result);
 	return result;
 }
-function subtract(num1, num2){
+function subtraction(num1, num2){
 	var result = num2 - num1;
 	console.log("Difference:"+result);
 	return result;
 }
-function multiply(num1,num2)
+function multiply(num1,num2,num3,num4)
 {
-	var result = num1*num2;
+	var result = num3*num2*num1*num4;
 	console.log("Multiplication:"+result);
 	return result;
 }
